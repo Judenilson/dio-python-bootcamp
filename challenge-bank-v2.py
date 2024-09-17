e3b6bfc4aedf55d42deff75a439bad231d6c883c
Judenilson Araujo Silva <judenilson@hotmail.com>
2024-09-17 08:24:14 -0300

    add str

--- challenge-bank-v2.py
+++ challenge-bank-v2.py
@@ -27,7 +27,10 @@ class Conta:
     
     @property
     def extrato(self):
-        return self.__extrato
+        return self.__extrato    
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
     def imprimir_extrato(self, saldo, /, *, extrato):
         """
