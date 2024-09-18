a5101a519c9821548fcc313a41327add293b70ac
Judenilson Araujo Silva <judenilson@hotmail.com>
2024-09-18 20:58:33 -0300

    fix setup

--- package-project/setup.py
+++ package-project/setup.py
@@ -14,7 +14,7 @@ setup(
     description="Image Processing",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Judenilson/dio-python-bootcamp/package-project/"
+    url="https://github.com/Judenilson/dio-python-bootcamp/package-project/",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
