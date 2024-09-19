2042abafc3aa1cec93b8a9429af9bfcc05b1e117
Judenilson Araujo Silva <judenilson@hotmail.com>
2024-09-18 21:19:14 -0300

    build and dist

--- package-project/setup.py
+++ package-project/setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="jd_image_processing",
+    name="jd-image-processing",
     version="0.0.1",
     author="judenilson",
     author_email="judenilson@hotmail.com",
